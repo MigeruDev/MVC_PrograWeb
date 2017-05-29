36d156724ca02c46003c9e6b633450ba28689934
TheWorstOne <mangel.maciasn@ucuenca.ec>
2017-05-29 13:58:40 -0500

    PersonaVO

--- src/Model/VO/PersonaVO.java
+++ src/Model/VO/PersonaVO.java
@@ -5,7 +5,40 @@ public class PersonaVO {
 	private String nombre;
 	private String apellido;
 	
-	public PersonaVO(){
-		
+	public PersonaVO(String id, String nombre, String apellido){
+		this.id= id;
+		this.nombre = nombre;
+		this.apellido = apellido;
 	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getNombre()+" "+this.getApellido();
+	}
+	
+	
 }
