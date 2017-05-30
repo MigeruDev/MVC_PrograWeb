5267c4f4c05cc70256e39a2665ea65ed7bc6911a
Andre Joseph Jaramillo <andre.joseph.jaramillo@gmail.com>
2017-05-30 16:52:46 -0500

    json

---
+++ src/Model/DAO/Database.java
@@ -0,0 +1,14 @@
+package Model.DAO;
+
+public class Database {
+	private String host;
+	private String user;
+	private String pass;
+	private String name;
+	private String port;
+	
+	private void cargar_configuracion(){
+		
+	}
+
+}
