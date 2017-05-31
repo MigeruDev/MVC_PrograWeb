102d8421934dbcf8f38d73c3b092bc648763dc4b
TheWorstOne <mangel.maciasn@ucuenca.ec>
2017-05-30 20:25:40 -0500

    view

--- src/View/Home.java
+++ src/View/Home.java
@@ -42,11 +42,12 @@ public class Home {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 598, 462);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		table = new JTable();
+		table.setBounds(10, 281, 562, 131);
 		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
@@ -55,8 +56,20 @@ public class Home {
 			new String[] {
 				"Id", "Nombre", "Apellido"
 			}
-		));
-		table.setBounds(10, 165, 414, 85);
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
 		frame.getContentPane().add(table);
 	}
 }
